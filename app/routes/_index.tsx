f706a54003e6fcdcab1f7e41b6a4ff9d0271343c
lirasusejdev <lis.amaral@grupoboticario.com.br>
2024-05-20 12:32:47 -0300

    testes de consumo de dados

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,30 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+    { title: 'New Remix App' },
+    { name: 'description', content: 'Welcome to Remix!' }
+  ]
+}
+
+export const loader = async () => {
+  const res = await fetch('https://fakestoreapi.com/products')
+  return res.json()
+}
 
 export default function Index() {
+  const products = useLoaderData()
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
+      <h1>Nossos Produtos</h1>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {products.map(product => (
+          <li key={product.id}>
+            <a href={`/products/${product.id}`}>{product.title}</a>
+          </li>
+        ))}
       </ul>
     </div>
-  );
+  )
 }
